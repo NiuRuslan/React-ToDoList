96a55b8bda3836f78c68dbbdadb297a6c66ba93c
NiuRuslan <niuruslan@gmail.com>
2020-04-03 13:18:24 +0300

    Updates

---
+++ precache-manifest.b74414ee748587b8a72c239fd785c789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a27d86e8335da3616e7724097a43d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee8994d17e8074762d56",
+    "url": "/static/css/main.1b46b547.chunk.css"
+  },
+  {
+    "revision": "0ad2bcd5cd8515847097",
+    "url": "/static/js/2.d4b00608.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4b00608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8994d17e8074762d56",
+    "url": "/static/js/main.27b883ac.chunk.js"
+  },
+  {
+    "revision": "6c725d5ce54ceabe8e25",
+    "url": "/static/js/runtime-main.55c2e157.js"
+  }
+]);
